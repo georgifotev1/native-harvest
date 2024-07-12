3c2a06ffafed08ded05616ec783bb6ee08f93ac7
georgifotev1 <georgifotev@protonmail.com>
2024-07-12 21:22:46 +0300

    added banner image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-
+import Image from "next/image";
+import background from "../public/images/jacqueline-o-gara-2sfoa14KlCU-unsplash.jpg"
 export default function Home() {
   return (
     <main className="px-8">
-      <div>Hello test</div>
+      <div className="h-screen overflow-hidden relative">
+          <Image src={background} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="Crops" className="object-cover h-full"/>
+          <div className="absolute top-0 left-0 h-full w-full bg-black/50"></div>
+      </div>
     </main>
   );
 }
