12a92f13bd4368861c46c5dab63c64047b71711d
georgifotev1 <georgifotev@protonmail.com>
2024-07-12 22:58:08 +0300

    Add sone ui components and a basic banner bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <main className="px-8">
       <div className="h-screen overflow-hidden relative">
-          <Image src={background} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="Crops" className="object-cover h-full"/>
+          <Image src={background} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="Crops" className="object-cover h-full absolute"/>
           <div className="absolute top-0 left-0 h-full w-full bg-black/50"></div>
+          <div className="absolute top-52 left-28 text-white">
+            <h1 className="text-7xl">Родна Продукция</h1>
+          </div>
       </div>
     </main>
   );
